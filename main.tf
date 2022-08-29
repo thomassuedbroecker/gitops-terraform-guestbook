56bb96a50444c8506df4751a97ae2693aa331bf6
thomassuedbroecker <thomas.suedbroecker@de.ibm.com>
2022-08-29 16:16:28 +0200

    add a custom module name

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 locals {
-  name          = "my-module"
+  name          = "guestbook-module"
   bin_dir       = module.setup_clis.bin_dir
   yaml_dir      = "${path.cwd}/.tmp/${local.name}/chart/${local.name}"
   service_url   = "http://${local.name}.${var.namespace}"
